317d55da921dcd9e1b5961c611e552db2d197b27
eldeveloper49 <44892517+eldeveloper49@users.noreply.github.com>
2018-11-16 11:25:24 +0100

    Add files via upload

---
+++ v_bandeau.php
@@ -0,0 +1,22 @@
+<div id="bandeau">
+<!-- Images En-tête -->
+<!--<img src="images/menuGauche.gif"	alt="Choisir" title="Choisir"/>-->
+<img id="logo" src="images/logo.png" width="200px">
+</div>
+<!--  Menu haut-->
+<ul id="menu">
+	<li><a href=""> Accueil </a></li>
+	<li><a href=""> Salle </a></li>
+	<li><a href=""> Reservation </a></li>
+</ul>
+
+
+
+<ul id="pied">
+	<li><a href=""> Mentions </a></li>
+	<li><a href=""> Contact </a></li>
+</ul>
+
+<ul id="login">
+	<li><a href=""> Se connecter </a></li>
+</ul>
